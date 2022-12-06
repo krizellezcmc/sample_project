941a149f075375589c23f0eaa4ad94bc83765934
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-12-06 15:38:56 +0800

    krizelle

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '28dd2d49c489e153ba05845276529309c8204be1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '28dd2d49c489e153ba05845276529309c8204be1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'krizellezcmc10/sample_project' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../krizellezcmc10/sample_project',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
